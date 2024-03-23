40fa02e38e49575b24089254d1495c5537b46c30
Patrick Heath <patheath@gmail.com>
2024-03-23 11:08:58 -0400

    first test

---
+++ main_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"bytes"
+	"io"
+	"os"
+)
+
+func TestMain(t *testing.T) {
+	mainStdout := captureStdout(main)
+
+	assert.Equal(t, mainStdout, "Hello, World!\n")
+}
+
+// Source https://gist.github.com/mindscratch/0faa78bd3c0005d080bf
+func captureStdout(f func()) string {
+	old := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+
+	f()
+
+	w.Close()
+	os.Stdout = old
+
+	var buf bytes.Buffer
+	io.Copy(&buf, r)
+	return buf.String()
+}
